27a34a066393ca3a2098df2111cd520392df05e8
Priya VR <priya.vr@experionglobal.com>
2016-01-12 13:39:13 +0530

    tableview created

---
+++ tableviewap/TableViewCell1.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1.swift
+//  tableviewap
+//
+//  Created by Priya VR on 1/12/16.
+//  Copyright (c) 2016 Priya VR. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
